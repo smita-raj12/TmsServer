90c459c88b4bc5a78e937613618f0a9ed78d0457
Smita Raj <smita.raj12@gmail.com>
2022-03-16 22:11:54 -0700

    make it better

--- startup/db.js
+++ startup/db.js
@@ -8,19 +8,19 @@ const mysql = require("mysql");
 //     database: "timeentrydatabase"
 //   });
 
-const db= mysql.createConnection({
-  host: "us-cdbr-east-05.cleardb.net",
-  user:"be737331350a0d",
-  password:"454fc2ea",
-  database: "heroku_22ab797459c1fe1"
-});
-
 // const db= mysql.createConnection({
-//     host: "database-1.cfdorcrhxxic.us-west-2.rds.amazonaws.com",
-//     user:"admin",
-//     password:"aarav2014",
-//     database: "database-1"
-//   });
+//   host: "us-cdbr-east-05.cleardb.net",
+//   user:"be737331350a0d",
+//   password:"454fc2ea",
+//   database: "heroku_22ab797459c1fe1"
+// });
+
+const db= mysql.createConnection({
+    host: "database-1.cfdorcrhxxic.us-west-2.rds.amazonaws.com",
+    user:"admin",
+    password:"aarav2014",
+    database: "database-1"
+  });
 
 module.exports = db;
 
